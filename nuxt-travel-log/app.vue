4712348e94e35ab209ee8a39b4f9084f567414d3
Abdullah Hussain <abdullah1234qq>
2025-06-18 18:06:02 +0500

    Initial commit

--- nuxt-travel-log/app.vue
+++ nuxt-travel-log/app.vue
@@ -3,6 +3,8 @@
       <h1 class="text-3xl font-bold underline">
     Hello world!
   </h1>
-  <button class="btn btn-secondary">Secondary</button>
+  <button class="btn btn-secondary">
+    View Map <Icon name="tabler:map"  />
+  </button>
   </div>
 </template>
