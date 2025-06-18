0ff28f6b86b6928850dffe8c6d39192dc658a609
Abdullah Hussain <abdullah1234qq>
2025-06-18 18:35:02 +0500

    Tnnitial commit

---
+++ nuxt-travel-log/Layouts/default.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { AppNavBar } from '#components';
+
+</script>
+
+<template>
+    <div class="flex min-h-screen flex-col">
+        <AppNavBarar/>
+        <main>
+            <slot/>
+        </main>
+    </div>
+</template>
---
+++ nuxt-travel-log/components/app/nav-bar.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="navbar bg-primary text-primary-content">
+  <button class="btn btn-ghost text-xl">Travel Log</button>
+</div>
+</template>
