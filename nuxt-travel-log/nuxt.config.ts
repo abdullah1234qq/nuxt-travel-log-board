e8e73a2a5dacad8b7fa9b091a8783836b5ca5b44
Abdullah Hussain <abdullah1234qq>
2025-06-18 17:26:31 +0500

    Initial commit

--- nuxt-travel-log/nuxt.config.ts
+++ nuxt-travel-log/nuxt.config.ts
@@ -1,3 +1,4 @@
+import tailwindcss from "@tailwindcss/vite";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
@@ -7,5 +8,10 @@ export default defineNuxtConfig({
     config: {
       standalone: false,
     },
+  },
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
   },
 });
