8751b272ff238b32f976bc0cc2042756d636e299
Abdullah Hussain <abdullah1234qq>
2025-06-18 17:31:04 +0500

    Initial commit

--- nuxt-travel-log/nuxt.config.ts
+++ nuxt-travel-log/nuxt.config.ts
@@ -4,6 +4,7 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
   modules: ["@nuxt/eslint"],
+  css: ['~/assets/css/main.css'],
   eslint: {
     config: {
       standalone: false,
